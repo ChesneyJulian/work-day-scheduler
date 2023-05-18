935a0133cfed1efb8403ccb71b4e752d35c094e8
Chesney Julian <chesney.julian724@gmail.com>
2023-05-18 18:10:54 -0400

    edit comments for specifity and clarity in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ $(function () {
     // splits id string at dash and grabs number which is at index 1
     var id = $(this).attr("id").split('-')[1];
     var rowHour = parseInt(id);
-    // conditional to color code depending on if current hour (hour) is <, >, or = to the rowHour
+    // conditional to color code for past, future, or present depending on if current hour (hour) is <, >, or = to the rowHour
     if (hour > rowHour) {
       $(this).addClass('past');
     } else if (hour < rowHour) {
@@ -22,16 +22,18 @@ $(function () {
       $(this).addClass('present');}
 
       // retrieve events saved in local storage and set them to the value of each corresponding timeBlock
-      // if the id matches the parentId key used in setting local storage, the text will appear. if not, it remains blank 
+      // each time block retrieves localStorage data with key of its own id 
     var text = JSON.parse(localStorage.getItem(id));
     $(this).children('.description').val(text);                       
   })
 
   // add event listener to all elements with class .saveBtn so that when clicked, the user input is saved to local storage with the key value defined by the parent container's id
  $(".saveBtn").on('click', function (event) {
+  // parentId refers to id of timeBlock - this will match the id variable used to set text of each timeblock in function above
     var parentId = ($(this).parent().attr('id').split('-')[1]);
     // must follow path from this = button to parent container to child with class of description
     var event = ($(this).parent().children('.description').val())
+    // sets parentId (will be the same as id in above function) as key and the entered text as the value within local storage
     localStorage.setItem(parentId, JSON.stringify(event));
   })
 });
